f213578ca938794feb4b154578fbefa3915948f0
shiranekaoru <126559076+shiranekaoru@users.noreply.github.com>
2024-06-21 15:15:06 +0900

    Update ContentView.swift

--- dialcrown_text/ContentView.swift
+++ dialcrown_text/ContentView.swift
@@ -8,19 +8,66 @@
 import SwiftUI
 import WatchConnectivity
 
+// カスタムのシード可能な乱数生成器
+struct SeedableRandomNumberGenerator: RandomNumberGenerator {
+    private var state: UInt64
+
+    init(seed: UInt64) {
+        self.state = seed
+    }
+
+    mutating func next() -> UInt64 {
+        state &+= 0x9E3779B97F4A7C15
+        var z = state
+        z = (z ^ (z >> 30)) &* 0xBF58476D1CE4E5B9
+        z = (z ^ (z >> 27)) &* 0x94D049BB133111EB
+        return z ^ (z >> 31)
+    }
+}
+
+struct ContentView1: View {
+    @State private var randomNumber: Int = 0
+
+    var body: some View {
+        VStack {
+            Text("Generated Random Number: \(randomNumber)")
+                .font(.largeTitle)
+                .padding()
+
+            Button(action: {
+                let seed = UInt64(DispatchTime.now().uptimeNanoseconds)
+                var generator = SeedableRandomNumberGenerator(seed: seed)
+                randomNumber = Int(generator.next() % 100 + 1)
+            }) {
+                Text("Generate Random Number with Time Seed")
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .padding()
+    }
+}
+
 struct ContentView: View {
     @State private var startFlag = false
     @State private var end = false
     @State private var startTime:Date = Date()
     @State private var CPM:String = ""
     @State var inputName = ""
+    @State private var taplocation:CGPoint = CGPoint()
+    @State private var filename:String = ""
     @ObservedObject private var connector = WatchConnector()
     var body: some View {
         VStack {
             
-            Text(connector.receivedMessage)
-                .bold()
-
+            TextField("ファイル名を入力してください", text:$filename)
+            Button("決定"){
+                
+                connector.setFileName(name: filename)
+            }
         }
         .padding()
     }
@@ -31,7 +78,7 @@ class CSVFileManager{
     var filename: String
     var fileManager: FileManager
     
-    var folder_name = "dial_crown/shirane"
+    var folder_name = "dial_crown/user"
    
     
     //初期化:filenameのファイルがなかった場合新たにファイルを作製
@@ -42,7 +89,7 @@ class CSVFileManager{
         let docPath = NSHomeDirectory() + "/Documents/" + folder_name
         let filePath = docPath + "/" + self.filename
         //csvデータに書き込むデータを定義
-        let csv = "user,method,sessionID,phrase,phraseID,keystroke,TIME,CPM,TER\r\n"
+        let csv = "phrase,phraseID,enter_log,enter_alllog,keystroke,TIME,CPM,VowelCnt,ConsCnt,TER,DeleteCnt,AveSpeed,AveRotationWhile\r\n"
         let data = csv.data(using: .utf8)
         
         let fileManager = FileManager.default
@@ -71,8 +118,7 @@ class CSVFileManager{
             // Unicode の U+FEFFは、表示がない文字。「ZERO WIDTH NO-BREAK SPACE」（幅の無い改行しない空白）
             strm.write(BOM, maxLength: 3)// UTF-8 の BOM 3バイト 0xEF 0xBB 0xBF 書き込み
             
-            let new_content = "user,method,sessionID,phrase,phraseID,keystroke,TIME,CPM,TER\r\n" + content
-            
+            let new_content = "phrase,phraseID,enter_log,enter_alllog,keystroke,TIME,CPM,VowelCnt,ConsCnt,TER,DeleteCnt,AveSpeed,AveRotationWhile\r\n" + content
             let data = new_content.data(using: .utf8)
             // string.data(using: .utf8)メソッドで文字コード UTF-8 の
             // Data 構造体を得る
@@ -124,8 +170,8 @@ class WatchConnector: NSObject,ObservableObject,WCSessionDelegate{
     @Published var timestamp = "0.0"
     @Published var count = 0
     @Published var file_name = ""
-    @Published var user = "ashizawa"
-    @Published var method = "layout_notouch_dial"
+    @Published var user = "matsuda"
+    @Published var method = "tc"
     @Published var sessionID = "0" //0:練習，１:本番
     override init() {
         super.init()
@@ -134,7 +180,10 @@ class WatchConnector: NSObject,ObservableObject,WCSessionDelegate{
             WCSession.default.activate()
         }
     }
-    
+    func setFileName(name:String){
+        file_name = name
+        self.receivedMessage = ""
+    }
     func session(
         _ session: WCSession,
         activationDidCompleteWith activationState: WCSessionActivationState,
@@ -153,24 +202,32 @@ class WatchConnector: NSObject,ObservableObject,WCSessionDelegate{
     
     func session(_ session: WCSession, didReceiveMessage message: [String: Any]) {
         print("didReceiveMessage: \(message)")
-
-        
-        let file_name = "pstudy_ashizawa_notouch_dial_s1.csv"
+//        let formatter = DateFormatter()
+//        
+////        formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "ydMMM", options: 0, locale: Locale(identifier: "ja_JP"))
+//        
+//        
+//        let file_name = formatter.string(from: Date())
         let file = CSVFileManager(filename: file_name)
         DispatchQueue.main.async {
             
             //csvファイルに書き込むために文字列を作成
             
-            self.receivedMessage += "\(self.user),"
-            self.receivedMessage += "\(self.method),"
-            self.receivedMessage += "\(self.sessionID),"
+            
             self.receivedMessage += "\(message["phrase"] as! String),"
             self.receivedMessage += "\(message["phraseID"] as! Int),"
+            self.receivedMessage += "\(message["enter_log"] as! String),"
+            self.receivedMessage += "\(message["enter_alllog"] as! String),"
             self.receivedMessage += "\(message["Keystroke"] as! Int),"
             self.receivedMessage += "\(message["Time"] as! String),"
             self.receivedMessage += "\(message["CPM"] as! String),"
+            self.receivedMessage += "\(message["VowelCnt"] as! Int),"
+            self.receivedMessage += "\(message["ConsCnt"] as! Int),"
             self.receivedMessage += "\(message["TER"] as! Double),"
-            self.receivedMessage += "\(message["DeleteCnt"] as! Double)\r\n"
+            self.receivedMessage += "\(message["MSD"] as! Double),"
+            self.receivedMessage += "\(message["DeleteCnt"] as! Double),"
+            self.receivedMessage += "\(message["AveSpeed"] as! [CGFloat]),"
+            self.receivedMessage += "\(message["AveRotationWhile"] as! [CGFloat])\r\n"
             print(self.receivedMessage)
             file.write(content:self.receivedMessage)
         }
